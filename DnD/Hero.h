2ec9dae89921794a01bb7239d54cd8a3649d7a50
IGOR <IGOR@DESKTOP-1BPEMD8>
2023-01-07 22:24:01 +0200

    вццв

--- DnD/Hero.h
+++ DnD/Hero.h
@@ -19,6 +19,7 @@ public:
 	Hero(std::string name,int value1, int value2,int value3,int value4)
 		:Name(name), Health(value1),dexterity(value2),
 		Intelligence(value3),charisma(value4) {}
-	
+	 
+
 
 };
