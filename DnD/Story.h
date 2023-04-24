659134485ad2f715e3ab2b485bd742d7f8846051
Виктор Яценко <pepsiboss4@gmail.com>
2023-04-25 02:56:40 +0300

    Story

---
+++ DnD/Story.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Story
+{
+public:
+	void PrintMassage(std::string massage);
+	int ArrayMassage(std::vector<int> arrayMassage);
+};
+
